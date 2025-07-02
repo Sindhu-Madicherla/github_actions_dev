55a4e51cdacab11879de2301557a4880d440d378
sindhu0302@gmail.com <sindhu0302@gmail.com>
2025-07-02 16:17:36 +0530

    Testing

--- tetss_2/test_my_module_2.py
+++ tetss_2/test_my_module_2.py
@@ -1,7 +1,7 @@
 from src.my_module import add, subtract
 
-def test_add():
+def test_add_1():
     assert add(2, 3) == 5
 
-def test_subtract():
+def test_subtract_1():
     assert subtract(5, 3) == 1
